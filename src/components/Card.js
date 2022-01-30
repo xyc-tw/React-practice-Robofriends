ade34c858dedfbeef62fbadc053bb1b230c7cfd1
xychen <mars_0608@hotmail.com>
2022-01-30 18:22:13 +0100

    publish gh-Pages and upload fonts

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+  //   const [name, email, id] = props; another way to deconstruct the props
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img
+        alt="robot"
+        src={`https://robohash.org/${id}`}
+        width={200}
+        height={200}
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
